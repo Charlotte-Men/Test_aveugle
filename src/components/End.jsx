53f46b42d5f271c0cb6601e46d06788c6188c1db
Charlotte Menard <charlotte-menard_student2021@wilder.school>
2021-11-29 21:50:55 +0100

    Fix App name + Favicon + reset game at the end + hover on buttons

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import styles from './End.module.css';
 import Glitter from '../assets/glitter.gif';
 
-function End () {
+function End ({ resetGame }) {
 
     // let navigate = useNavigate();
     // function playGame(){
@@ -16,10 +16,10 @@ function End () {
     <h1 className={styles.EndTitle}>BRAVO ! </h1>
         <img src={Glitter} alt="homme jetant des paillettes" className={styles.EndGif}/>
 
-    <h2 className={styles.EndH2}>Vous êtes arrivés à la fin ! </h2>
+    <h2 className={styles.EndH2}>C'est la fin ! </h2>
     <p className={styles.EndText}>Vous n'avez pas trouvé toutes les bonnes réponses ? </p>
     <p className={styles.EndText}>Vous pouvez recommencer ! </p>
-    {/* <button className={styles.EndButton} onClick={playGame}>Play Again</button> */}
+    <button className={styles.EndButton} onClick={resetGame}>Jouer encore</button>
 </div>
     )
 }
